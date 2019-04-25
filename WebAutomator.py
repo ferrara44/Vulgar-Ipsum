469d6ddd97edff58624bf96d2b5ee8866fb64866
Facundo Ferrara <ferraraphys@gmail.com>
2019-04-25 07:21:48 -0300

    Headless Firefox Working

--- WebAutomator.py
+++ WebAutomator.py
@@ -5,16 +5,17 @@ import psutil
 options = Options()
 gecko = 'geckodriver.exe'
 
-
-browser = webdriver.Firefox()
+options.add_argument('-headless')
+browser = webdriver.Firefox(options=options)
+print("Firefox Initialized successfully")
 browser.get('http://vulgarlang.com')
-
+print("Accessing VulgarLang")
 submit = browser.find_element_by_xpath("/html/body/div[5]/div/div[2]/div/div/main/article/div[1]/div[1]/center/button")
-
+print("Button Found")
 submit.click()
-
+print("Button Clicked")
 wordList = browser.find_element_by_xpath("/html/body/div[5]/div/div[2]/div/div/main/article/div[1]/div[3]/div[1]/div[4]/ul").text
-
+print("WordList Copied")
 browser.close()
 for proc in psutil.process_iter():
     # check whether the process name matches
